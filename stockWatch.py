50e99dac51f42406196b14eb47001fae8ac0fe6e
Michael Wood <mikewood95@gmail.com>
2017-06-28 05:49:22 +0000

    Addition of stock price. Addition of 30sec delay in refresh. General visual formatting.

--- stockWatch.py
+++ stockWatch.py
@@ -1,22 +1,27 @@
 import ystockquote
+import time
+import os
 everything = dir(ystockquote)
 watchlist = ('QTNA', 'AMD', 'CYTR', '^IXIC')
 while True:
 	for symbol in watchlist:
 		print symbol + " - Comany Name:	" + ystockquote.get_company_name(symbol)
+		print "Price:			" + ystockquote.get_last_trade_price(symbol)
 		print "Volume:			" + ystockquote.get_volume(symbol)
 		print "Daily Average Volume:	" + ystockquote.get_average_daily_volume(symbol)
 		# Checks to see if volume data is present to avoid any errors
 		if ystockquote.get_average_daily_volume(symbol) != 'N/A':
 			# Ratio of current share volume to average share volume 
 			volume_ratio = float(ystockquote.get_volume(symbol)) / float(ystockquote.get_average_daily_volume(symbol))
+			print "Volume Ratio:		", volume_ratio
 		else:
 			print "Volume Ratio:		Data Unavailable"
-		print "Volume Ratio:		", volume_ratio
+
 		print "Short Ratio:		" + ystockquote.get_short_ratio(symbol)
 		print "52 Week High:		" + ystockquote.get_52_week_high(symbol)
 		print "52 Week Low:		" + ystockquote.get_52_week_low(symbol)
 		print "\n"	
-		
+	os.system('clear') # System call to clear screen after cycling through stock data
+	time.sleep(30)	
 		
 		
