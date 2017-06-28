74e27ad12b22e1ed035735c81c9a053313d15227
Michael Wood <mikewood95@gmail.com>
2017-06-28 17:51:32 +0000

    Addition of some condtitional color text

--- stockWatch.py
+++ stockWatch.py
@@ -8,6 +8,12 @@ Discription:	This program keeps track of a number of stocks in a users watch lis
 import ystockquote
 import time
 import os
+def green_text(string, data):
+	print "\033[32m" + string, data, "\033[0m"
+def red_text(string, data):
+	print "\033[31m" + string, data, "\033[0m"
+def blue_text(string, data):
+	print "\033[34m" + string, data, "\033[0m"
 
 # List all stocks that the user wishes to keep track of
 watchlist = ('QTNA', 'AMD', 'CYTR', '^IXIC')
@@ -17,26 +23,47 @@ watchlist = ('QTNA', 'AMD', 'CYTR', '^IXIC')
 # For loop to fetch and display stock information
 while True:
 	for symbol in watchlist:
-		print symbol + " - Comany Name:	" + ystockquote.get_company_name(symbol)
-		print "Price:			" + ystockquote.get_last_trade_price(symbol)
-		print "Percent change:		" + ystockquote.get_change_percent(symbol)
-		print "Todays Return:		",  (float(ystockquote.get_last_trade_price(symbol)) - float(ystockquote.get_today_open(symbol)))
-		print "Open:			" + ystockquote.get_today_open(symbol)
-		print "Volume:			" + ystockquote.get_volume(symbol)
-		print "Daily Average Volume:	" + ystockquote.get_average_daily_volume(symbol)
+		price = float(ystockquote.get_last_trade_price(symbol))
+		company_name = ystockquote.get_company_name(symbol)
+		percent_change = (ystockquote.get_change_percent(symbol))
+		open_price = float(ystockquote.get_today_open(symbol))
+		todays_return = (price - open_price)
+		vol = ystockquote.get_volume(symbol)
+		daily_average_vol = ystockquote.get_average_daily_volume(symbol)
+		short_ratio = ystockquote.get_short_ratio(symbol)
+		ft_week_high = ystockquote.get_52_week_high(symbol)
+		ft_week_low = ystockquote.get_52_week_low(symbol)
+
+		print symbol + " - Comany Name:	" + company_name 
+		blue_text("Price:			", price)
+		if(price > open_price):
+			green_text("Percent change:		", percent_change) 
+			green_text("Todays Return:		", todays_return) 		
+		else:
+			red_text("Percent change:		", percent_change) 
+			red_text("Todays Return:		", todays_return) 		
+		print "Open:			", open_price 
+		print "Volume:			" + vol
+		print "Daily Average Volume:	" + daily_average_vol
 		# Checks to see if volume data is present to avoid any errors
 		if ystockquote.get_average_daily_volume(symbol) != 'N/A':
 			# Ratio of current share volume to average share volume 
 			volume_ratio = float(ystockquote.get_volume(symbol)) / float(ystockquote.get_average_daily_volume(symbol))
-			print "Volume Ratio:		", volume_ratio
+			if(volume_ratio > 1):
+
+				green_text("Volume Ratio:		", volume_ratio)
+			else:
+				red_text("Volume Ratio:		", volume_ratio)
+
 		else:
 			print "Volume Ratio:		Data Unavailable"
 
-		print "Short Ratio:		" + ystockquote.get_short_ratio(symbol)
-		print "52 Week High:		" + ystockquote.get_52_week_high(symbol)
-		print "52 Week Low:		" + ystockquote.get_52_week_low(symbol)
+		print "Short Ratio:		" + short_ratio 
+		print "52 Week High:		" + ft_week_high 
+		print "52 Week Low:		" + ft_week_low
 		print "\n"	
+	time.sleep(5)
 	os.system('clear') # System call to clear screen after cycling through stock data
-	time.sleep(10)	
+
 		
 		
