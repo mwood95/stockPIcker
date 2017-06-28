32d2b11b05b24773755ad1605383264bb1a6259f
Michael Wood <mikewood95@gmail.com>
2017-06-28 04:54:10 +0000

    Refinement of what stock data to list. Insertion of volume to daily volume ratio.

--- stockWatch.py
+++ stockWatch.py
@@ -1,14 +1,20 @@
 import ystockquote
 everything = dir(ystockquote)
-watchlist = ('QTNA', 'AMD', 'CYTR', 'IXIC')
+watchlist = ('QTNA', 'AMD', 'CYTR', '^IXIC')
 while True:
 	for symbol in watchlist:
-		print "Comany Name: " + ystockquote.get_company_name(symbol)
-		print "Volume: " + ystockquote.get_volume(symbol)
-		print "Daily Average Volume: " + ystockquote.get_average_daily_volume(symbol)
-		print "Price Book: " + ystockquote.get_price_book(symbol)
-		print "Ticker Trend: " + ystockquote.get_ticker_trend(symbol)
-		print "Short Ratio: " + ystockquote.get_short_ratio(symbol)
-		print "52 Week High: " + ystockquote.get_52_week_high(symbol)
-		print "52 Week Low: " + ystockquote.get_52_week_low(symbol)
-		print " "
+		print symbol + " - Comany Name:	" + ystockquote.get_company_name(symbol)
+		print "Volume:			" + ystockquote.get_volume(symbol)
+		print "Daily Average Volume:	" + ystockquote.get_average_daily_volume(symbol)
+		if ystockquote.get_average_daily_volume(symbol) != 'N/A':
+			volume_ratio = float(ystockquote.get_volume(symbol)) / float(ystockquote.get_average_daily_volume(symbol))
+		else:
+			print "Volume Ratio:		Data Unavailable"
+		print "Volume Ratio:		", volume_ratio
+		print "Short Ratio:		" + ystockquote.get_short_ratio(symbol)
+		print "52 Week High:		" + ystockquote.get_52_week_high(symbol)
+		print "52 Week Low:		" + ystockquote.get_52_week_low(symbol)
+		print "\n"	
+		
+		
+		
